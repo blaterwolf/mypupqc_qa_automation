2b8e660bc799cd1a8dc782b34a8dae6316e7db8a
jarret20 <jarretbenedict@gmail.com>
2023-06-28 21:12:06 +0800

    Minor Update on MQC-20

--- tests/omsss/mqc19.test.js
+++ tests/omsss/mqc19.test.js
@@ -20,42 +20,45 @@ describe('MQC-19: Patient Information Page', () => {
         const loggedInElementXPath = '/html/body/div/div[3]/div/div/div[1]/div/div'
         const loggedInElement = await driver.wait(
             until.elementLocated(By.xpath(loggedInElementXPath)),
-            10000
+            5000
         )
 
         expect(loggedInElement).toBeDefined()
     }, 30000)
 
-    test('A dropdown with options "Patient Information" and "Health History" is expected', async () => {
+    test('A dropdown with options "Patient Information" is expected and Should load the Patient Information Page', async () => {
+        // somehow mabagal mag-load so wait for 5 seconds to load
+        await driver.sleep(5000)
+
+        // const health
+
         const healthInfoSidebarXPath =
-            '/html/body/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div/ul/div[1]/div[2]/div/div/div/li[16]'  
+            '/html/body/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div/ul/div[1]/div[2]/div/div/div/li[16]/a'
 
         await driver.findElement(By.xpath(healthInfoSidebarXPath)).click()
-    }, 30000)
 
-    test('Should load the Patient Information Page', async () => {
-        const patientoInfoPageXPath =
-            '//*[@id="sidebarHealthInfo"]/ul/li[1]/a'
+        const patientInfoXpath = '/html/body/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div/ul/div[1]/div[2]/div/div/div/li[16]/div/ul/li[1]/a'
 
-        await driver.findElement(By.xpath(patientoInfoPageXPath)).click()
-        await driver.sleep(10000)
+        await driver.findElement(By.xpath(patientInfoXpath)).click()
+
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should load the data of the student at the side of the form', async () => {
-        const studentFormTypeXPath = '//*[@id="layout-wrapper"]/div[3]/div[1]/div/div[3]/div[1]/div/div/div'
+        const studentFormTypeXPath =
+            '//*[@id="layout-wrapper"]/div[3]/div[1]/div/div[3]/div[1]/div/div/div'
 
         await driver.findElement(By.xpath(studentFormTypeXPath)).click()
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Users are expected to input their patient information', async () => {
-        
         const contactPerson = 'Dobert Rodomal'
         const contactNumber = '09629437813'
         const email = 'dobertrodomal@gmail.com'
         const address = 'New York Street San Rafael Talisay Cebu VI'
         const philInfo = '123'
-        const fileInput = await driver.findElement(By.xpath(fileInputXPath));
+        const fileInput = await driver.findElement(By.xpath(fileInputXPath))
 
         const contactPersonXPath = '//*[@id="emergency_contact_name"]'
         const contactNumberXPath = '//*[@id="emergency_contact_number"]'
@@ -63,23 +66,26 @@ describe('MQC-19: Patient Information Page', () => {
         const addressXPath = '//*[@id="emergency_contact_address"]'
         const philInfoXPath = '//*[@id="philhealth_number"]'
         const fileInputXPath = '//*[@id="filepond--drop-label-maq8ug8y0"]'
-        
+
         await driver.findElement(By.xpath(contactPersonXPath)).sendKeys(contactPerson)
         await driver.findElement(By.xpath(contactNumberXPath)).sendKeys(contactNumber)
         await driver.findElement(By.xpath(emailXPath)).sendKeys(email)
         await driver.findElement(By.xpath(addressXPath)).sendKeys(address)
         await driver.findElement(By.xpath(philInfoXPath)).sendKeys(philInfo)
-        await driver.get('https://2.bp.blogspot.com/-POt1ik4WGVw/VtWlmzbrasI/AAAAAAAABAc/gWTVx0Rg3GA/s640/Philhealth.jpg').sendKeys(fileInput)
+        await driver
+            .get(
+                'https://2.bp.blogspot.com/-POt1ik4WGVw/VtWlmzbrasI/AAAAAAAABAc/gWTVx0Rg3GA/s640/Philhealth.jpg'
+            )
+        await driver.findElement(By.xpath(fileInputXPath)).sendKeys(fileInput) 
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to Update Patient Information', async () => {
         const updateButtonXPath = '//*[@id="update_patient_information"]'
 
         await driver.findElement(By.xpath(updateButtonXPath)).click()
-
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to see two buttons on the Philhealth ID section: one for viewing and deleting', async () => {
@@ -87,7 +93,7 @@ describe('MQC-19: Patient Information Page', () => {
 
         await driver.findElement(By.xpath(twoButtonsXPath))
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to delete the currently uploaded philhealth ID using the delete button and buttons on the Philhealth ID should be gone.', async () => {
@@ -95,7 +101,7 @@ describe('MQC-19: Patient Information Page', () => {
 
         await driver.findElement(By.xpath(deleteIDXPath))
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to click and view Currently Uploaded Philhealth ID', async () => {
@@ -103,72 +109,57 @@ describe('MQC-19: Patient Information Page', () => {
 
         await driver.findElement(By.xpath(viewIDXPath)).click()
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to click on Immunization', async () => {
-        const ImmunizationXPath = '/html/body/div/div[3]/div[1]/div/div[3]/div[2]/div/div[1]/ul/li[2]/a'
+        const ImmunizationXPath =
+            '/html/body/div/div[3]/div[1]/div/div[3]/div[2]/div/div[1]/ul/li[2]/a'
 
         await driver.findElement(By.xpath(ImmunizationXPath)).click()
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to upload Immunization Card', async () => {
         const uploadImmunizationXPath = '//*[@id="consultation_reason"]'
 
-        const fileInput = await driver.findElement(By.xpath(fileInputXPath));
+        const fileInput = await driver.findElement(By.xpath(fileInputXPath))
         const fileInputXPath = '//*[@id="filepond--drop-label-zrge5crqf"]'
 
-        await driver.findElement(By.xpath(uploadImmunizationXPath).get('https://www.gannett-cdn.com/presto/2021/03/08/NSTN/52a9d0b0-3497-48f9-a5b1-f5c87f3c9e0f-GROCERY1.jpg?crop=2559,1440,x0,y130&width=2559&height=1440&format=pjpg&auto=webp')).sendKeys(fileInput)
+        await driver
+            .findElement(
+                By.xpath(uploadImmunizationXPath).get(
+                    'https://www.gannett-cdn.com/presto/2021/03/08/NSTN/52a9d0b0-3497-48f9-a5b1-f5c87f3c9e0f-GROCERY1.jpg?crop=2559,1440,x0,y130&width=2559&height=1440&format=pjpg&auto=webp'
+                )
+            )
+            .sendKeys(fileInput)
 
-        
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to see two buttons on the Immunization section: one for viewing and deleting', async () => {
-        const reasonText = 'Bullying... saket sad 😔'
-        const consultationReasonXPath = '//*[@id="consultation_reason"]'
+        const vaxCardButtonsXPath = '//*[@id="show_vaxcard_button"]'
 
-        await driver.findElement(By.xpath(consultationReasonXPath)).sendKeys(reasonText)
+        await driver.findElement(By.xpath(vaxCardButtonsXPath))
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to click and view Currently Uploaded Vaccination Card', async () => {
-        const reasonText = 'Bullying... saket sad 😔'
-        const consultationReasonXPath = '//*[@id="consultation_reason"]'
+        const viewVaxCardXPath = '//*[@id="show_vaxcard_button"]/button[1]'
 
-        await driver.findElement(By.xpath(consultationReasonXPath)).sendKeys(reasonText)
+        await driver.findElement(By.xpath(viewVaxCardXPath)).click()
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
     test('Should be able to delete the currently uploaded Vaccination Card using the delete button and buttons on the Vaccination Card should be gone.', async () => {
-        const reasonText = 'Bullying... saket sad 😔'
-        const consultationReasonXPath = '//*[@id="consultation_reason"]'
+        const deleteVaxCardXPath = '//*[@id="show_vaxcard_button"]/button[2]'
 
-        await driver.findElement(By.xpath(consultationReasonXPath)).sendKeys(reasonText)
+        await driver.findElement(By.xpath(deleteVaxCardXPath)).click()
 
-        await driver.sleep(10000)
+        await driver.sleep(5000)
     }, 30000)
 
-    test('Should be able to Update Patient Information', async () => {
-        const submitButtonXPath = '//*[@id="addGuidanceAppointment"]'
-
-        await driver.findElement(By.xpath(submitButtonXPath)).click()
-
-        const successMessageXPath = '/html/body/div[2]/div/div[2]/div/div/p'
-
-        const successMessage = await driver.wait(
-            until.elementLocated(By.xpath(successMessageXPath)),
-            10000
-        )
-
-        expect(successMessage).toBeDefined()
-
-        const successMessageText = await successMessage.getText()
-
-        expect(successMessageText).toEqual('You have successfully added a Guidance Case!')
-    })
 })
--- tests/omsss/mqc20.test.js
+++ tests/omsss/mqc20.test.js
@@ -1,8 +1,8 @@
-const { Builder, By, until } = require('selenium-webdriver')
+const { Builder, By, until, WebElement } = require('selenium-webdriver')
 const { loginToStudent } = require('../../home/login')
 const firefoxOptions = require('../../helpers/firefoxOptions')
 
-describe('MQC-19: Patient Information Page', () => {
+describe('MQC-20: Health History Page', () => {
     let driver
 
     beforeAll(async () => {
@@ -27,20 +27,82 @@ describe('MQC-19: Patient Information Page', () => {
     }, 30000)
 
     test('A dropdown with options "Patient Information" and "Health History" is expected', async () => {
+        await driver.sleep(5000)
+
         const healthHistorySidebarXPath ='/html/body/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div/ul/div[1]/div[2]/div/div/div/li[16]'  
 
         await driver.findElement(By.xpath(healthHistorySidebarXPath)).click()
         
         await driver.sleep(5000)
-    }, 30000)
+    }, 15000)
 
     test('Should load the Health History Page', async () => {
+        await driver.sleep(5000)
+
         const healthHistoryPageXPath =
             '//*[@id="sidebarHealthInfo"]/ul/li[2]/a'
 
         await driver.findElement(By.xpath(healthHistoryPageXPath)).click()
 
         await driver.sleep(5000)
-    }, 30000)
+    }, 15000)
 
+    test('Should be able to click and choose in "Medical History"', async () => {
+        
+        const checkboxXPath = '//*[@id="#medical_history"]'
+        const checkbox = await driver.findElement(By.xpath(checkboxXPath))
+        await checkbox.click()
+
+        await driver.sleep(5000)
+    }, 30000) 
+
+    test('Should be able to click and choose in "Social History"', async () => {
+        
+        const checkboxXPath = '//*[@id="alcohol_history"]'
+
+        const checkbox = await driver.findElement(By.xpath(checkboxXPath))
+        await checkbox.click()
+
+        await driver.sleep(5000)
+    }, 30000) 
+    
+    test('Should be able to add text in "Allergy"', async () => {
+
+    const allergy = 'wasp/bee stings'
+        const allergyXPath =
+            '//*[@id="allergy"]'
+        
+        await driver.findElement(By.xpath(allergyXPath)).sendKeys(allergy)
+
+        await driver.sleep(5000)
+    }, 15000)
+
+    test('Should be able to add text in "Family History"', async () => {
+
+        const familyHistory = 'Asthma'
+        const familyHistoryXPath ='//*[@id="family_history"]'
+            
+        await driver.findElement(By.xpath(familyHistoryXPath)).sendKeys(familyHistory)
+        await driver.sleep(5000)
+    }, 15000)
+
+    test('Should be able to add text in "Medication"', async () => {
+
+        const medications = 'Cocaine'
+        const medicationsXPath ='//*[@id="medications"]'
+            
+        await driver.findElement(By.xpath(medicationsXPath)).sendKeys(medications)
+        await driver.sleep(5000)
+
+        const checkboxXPath = '//*[@id="privacy"]';
+        const checkbox = await driver.findElement(By.xpath(checkboxXPath));
+
+        await driver.executeScript("arguments[0].scrollIntoView(true);", checkbox);
+        await driver.sleep(5000)
+
+        await checkbox.click();
+        await driver.sleep(5000)
+    }, 30000)
+    
+    
 })
